1990d8b55b44e5f5728d4b956faa8d2fa4850e55
yathartharastogi <yras279@gmail.com>
2026-09-05 07:26:13 +0530

    Add Netlify redirect

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
